127a1e112149c176664b7dbf0eaf1f72e39755b4
QWAR-svg <zaz765831@gmail.com>
2021-06-23 14:37:06 +0300

    Values

---
+++ JsFolder/value.js
@@ -0,0 +1,15 @@
+let a = 42;
+let b = a;
+
+b++;
+console.log(a);
+console.log(b);
+
+let c = [1, 2, 3];
+let q = c; // Neet to use concat()
+q.push(6);
+
+// let c = [1, 2 ,3 ,4]
+
+console.log(c);
+console.log(q);
